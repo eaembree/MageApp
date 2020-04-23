e2ddbf83ea87d3ae6e596092fc27ed31eec694e7
Eric Embree <eaembree@gmail.com>
2020-04-23 06:50:59 -0500

    Mage button samples

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MageButton from './MageButton';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+      <MageButton></MageButton>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ Styles.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export const MageButtonStyle = StyleSheet.create({
+    btn: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: '#a32fa3',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#a32fa3'
+    },
+    btnText: {
+        color: '#ffc300',
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+});
+
+export const MageButtonInvStyle = StyleSheet.create({
+    btn: {
+        padding: 10,
+        margin: 10,
+        backgroundColor: '#ffc300',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#a32fa3'
+    },
+    btnText: {
+        color: '#a32fa3',
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+});
