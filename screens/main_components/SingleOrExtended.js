46e6b26ca9bf97a6e15669bd7552e844aab024c2
Eric Embree <eaembree@gmail.com>
2020-05-05 07:47:45 -0500

    Checks on single/extended buttons

--- screens/main_components/SingleOrExtended.js
+++ screens/main_components/SingleOrExtended.js
@@ -1,11 +1,78 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons'
 
 import {
     MageButtonStyle as btn,
-    MageButtonOutlineStyle as btnOut,
-    MageButtonInvStyle as btnInv
+    MageButtonOutlineStyle as btnOut
 } from '../../Styles'
+
+export function SingleOrExtended({ type, setSingle, setExtended }) {
+
+    const singleCallback = setSingle;
+    const extendedCallback = setExtended;
+
+    let leftBtnStyle, leftTextStyle, leftTextColor, leftCheck;
+    let rightBtnStyle, rightTextStyle, rightTextColor, rightCheck;
+
+    if (type == 'single') {
+        leftBtnStyle = btn.btn;
+        leftTextStyle = btn.btnText;
+        leftTextColor = btn.textColor;
+        leftCheck = true;
+        rightBtnStyle = btnOut.btn;
+        rightTextStyle = btnOut.btnText;
+        rightTextColor = btnOut.textColor;
+        rightCheck = false;
+    }
+    else {
+        leftBtnStyle = btnOut.btn;
+        leftTextStyle = btnOut.btnText;
+        leftTextColor = btnOut.textColor;
+        leftCheck = false;
+        rightBtnStyle = btn.btn;
+        rightTextStyle = btn.btnText;
+        rightTextColor = btn.textColor;
+        rightCheck = true;
+    }
+
+    return (
+        <View style={[styles.container, { marginTop: 25, marginBottom: 20 }]}>
+            <TouchableOpacity
+                style={[leftBtnStyle, styles.leftButton]}
+                onPress={singleCallback}
+            >
+                <CheckOrNo text="Single Action"
+                    showCheck={leftCheck}
+                    stylesArray={[leftTextStyle, leftTextColor]}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={[rightBtnStyle, styles.rightButton]}
+                onPress={extendedCallback}
+            >
+                <CheckOrNo text="Extended Action"
+                    showCheck={rightCheck}
+                    stylesArray={[rightTextStyle, rightTextColor]}
+                />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+function CheckOrNo({showCheck, stylesArray, text}) {
+    console.log(stylesArray)
+    let result = <Text style={stylesArray}>{text}</Text>
+
+    if(showCheck){
+        result =
+            <Text style={stylesArray}>
+                <FontAwesome5 name="check" /> {text}
+            </Text>
+    }
+
+    return result;
+}
 
 const styles = StyleSheet.create({
     container: {
@@ -29,49 +96,3 @@ const styles = StyleSheet.create({
         marginLeft: 0
     }
 });
-
-export function SingleOrExtended({ type, setSingle, setExtended }) {
-
-    const singleCallback = setSingle;
-    const extendedCallback = setExtended;
-
-    let leftBtnStyle, leftTextStyle, leftTextColor;
-    let rightBtnStyle, rightTextStyle, rightTextColor;
-
-    if (type == 'single') {
-        leftBtnStyle = btn.btn;
-        leftTextStyle = btn.btnText;
-        leftTextColor = btn.textColor;
-        rightBtnStyle = btnOut.btn;
-        rightTextStyle = btnOut.btnText;
-        rightTextColor = btnOut.textColor;
-    }
-    else {
-        leftBtnStyle = btnOut.btn;
-        leftTextStyle = btnOut.btnText;
-        leftTextColor = btnOut.textColor;
-        rightBtnStyle = btn.btn;
-        rightTextStyle = btn.btnText;
-        rightTextColor = btn.textColor;
-    }
-
-
-
-
-    return (
-        <View style={[styles.container, { marginTop: 25, marginBottom: 20 }]}>
-            <TouchableOpacity
-                style={[leftBtnStyle, styles.leftButton]}
-                onPress={singleCallback}
-            >
-                <Text style={[leftTextStyle, leftTextColor]}>Single Action</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={[rightBtnStyle, styles.rightButton]}
-                onPress={extendedCallback}
-            >
-                <Text style={[rightTextStyle, rightTextColor]}>Extended Action</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
