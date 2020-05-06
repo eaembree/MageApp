8f6f8e7796561d706b6e06fa73a4d4aa29fa3f74
Eric Embree <eaembree@gmail.com>
2020-05-06 17:25:30 -0500

    Cleaning

--- app/App.js
+++ app/App.js
@@ -1,21 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Platform, YellowBox } from 'react-native';
-import MageButtonSample from './MageButtonSample';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCoffee, faDiceD20, faQuestionCircle, faCog, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { StyleSheet, StatusBar, Platform, YellowBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-import BotchTensButtons from './BotchTensButtons';
-
-import BtnTestScreen from './screens/BtnTest';
-import ModalTestScreen from './screens/ModalTest';
 import MainScreen from './screens/Main';
 import SettingsScreen from './screens/Settings'
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-//const Stack = createStackNavigator();
-
 import { SettingsContext, TensOptions, BotchOptions } from './SettingsData';
 
 YellowBox.ignoreWarnings([
@@ -25,27 +15,7 @@ YellowBox.ignoreWarnings([
 ])
 
 
-//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-//const Tab = createBottomTabNavigator();
-
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-
 const Tab = createMaterialTopTabNavigator();
-
-function tabBarIcon({ focused, color, size }) {
-  let iconName;
-
-  if (route.name === 'Home') {
-    iconName = focused
-      ? faCoffee
-      : faDiceD20
-  } else if (route.name === 'BtnTest') {
-    iconName = focused ? faCog : faCheck;
-  }
-
-  // You can return any component that you like here!
-  return <FontAwesomeIcon icon={iconName} />;
-}
 
 const statusBarPadding = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
 
@@ -78,41 +48,6 @@ function App() {
   }
 
   return (
-    // <FontAwesomeIcon icon={ faCheck } />
-
-    // <NavigationContainer>
-    //   <Tab.Navigator
-    //   screenOptions={({ route }) => ({
-    //     tabBarIcon: ({ focused, color, size }) => {
-    //       let iconName;
-
-    //       if (route.name === 'Home') {
-    //         iconName = focused
-    //           ? faCoffee
-    //           : faDiceD20
-    //       } else if (route.name === 'BtnTest') {
-    //         iconName = focused ? faCog : faCheck;
-    //       }
-
-    //       // You can return any component that you like here!
-    //       return <FontAwesomeIcon icon={ iconName } />;
-    //     }
-    //   })}
-    //   >
-    //     <Tab.Screen name="Home" component={HomeScreen} />
-    //     <Tab.Screen name="BtnTest" component={BtnTestScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
-
-    //// With Status bar hidden
-    // <NavigationContainer>
-    //   <StatusBar hidden/>
-    //   <Tab.Navigator>
-    //     <Tab.Screen name="Home" component={HomeScreen} />
-    //     <Tab.Screen name="BtnTest" component={BtnTestScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
-
     <SettingsContext.Provider value={initialSettingsState}>
       <NavigationContainer styles={styles.baseComponent}>
         <Tab.Navigator>
